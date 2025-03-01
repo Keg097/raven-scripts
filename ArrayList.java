ff49416adb5cb19f481cf0506ca27927625434ba
Keg097 <davidchil2007@gmail.com>
2025-02-28 22:12:09 -0500

    Update Scaffold rotation logic to adjust slider threshold for improved functionality

--- ArrayList.java
+++ ArrayList.java
@@ -227,8 +227,8 @@ void updateSliders() {
         setDataArray("InvMove", "InvWalk", "Inventory", new String[]{"Disabled", "Hypixel", "Blink", "Close"});
 
         setDataArray("Scaffold", "Scaffold", "Rotation", new String[]{"None", "Smooth", "Spin"});
-        if (modules.getSlider("Scaffold", "Rotation (fake)") > 0) { // Rotation (fake) slider past None mode
-            setDataArray("Scaffold", "Scaffold", "Rotation (fake)", new String[]{"None", "Smooth", "Spin"});
+        if (modules.getSlider("Scaffold", "Rotation (fake)") > 1) { // Rotation (fake) slider past None mode
+            setDataArray("Scaffold", "Scaffold", "Rotation", new String[]{"None", "Smooth", "Spin"});
         } else {
             if (modules.getButton("Scaffold", "Jump facing forward")) {
                 setDataArray("Scaffold", "Scaffold", "Rotation", new String[]{"ResetStrict", "ResetSimple", "ResetLazy", "ResetOffset"});
